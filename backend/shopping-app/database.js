81b391197817270934d50c22fad9d0f129eafee3
srujanavanama <srujanavanama@gmail.com>
2021-07-23 15:01:50 +1000

    Signup, Login, Add to cart and Place Order functionalities added

---
+++ backend/shopping-app/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const db = mongoose
+    .connect(process.env.MONGODB_URI, 
+        {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('DB connected')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+module.exports = db;
---
+++ frontend/shopping-app/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer navbar text-center">
+            <div className="container">
+                <span className="text-muted">
+                    <span className="material-icons align-middle">copyright</span>
+                    All rights reserved - Srujana Vanama
+                </span>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
